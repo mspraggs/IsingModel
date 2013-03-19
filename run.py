b9728ffa931f2f761b49e1cc5194c193b45b82ef
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-19 21:18:28 +0000

    Changed number of iterations in runs and changed initial state to 0

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ import os
 Temps = [0.5*x for x in range(10)]
 
 for T in Temps:
-    L = lattice.Lattice(n = 100,T=T)
+    L = lattice.Lattice(n = 100,T=T,state=0)
     averages = []
-    for i in xrange(1000):
+    for i in xrange(100):
         print("Temperature: %f Step: %d" % (T,i))
         L.step()
         averages.append(L.spinaverage())
