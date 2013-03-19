7bcf1afb7b17a5c95d8ac42ececa3fd1b7a17ecd
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-19 21:16:16 +0000

    Altered strings in run to contain format string characters

--- run.py
+++ run.py
@@ -8,10 +8,11 @@ for T in Temps:
     L = lattice.Lattice(n = 100,T=T)
     averages = []
     for i in xrange(1000):
+        print("Temperature: %f Step: %d" % (T,i))
         L.step()
         averages.append(L.spinaverage())
 
-    fileio.writedata("results/Temperature="+str(T)+".txt",averages)
+    fileio.writedata("results/Temperature=%f.txt" % T,averages)
     os.system("git add *.txt")
-    os.system("git commit -am 'Added results for T="+T+"'")
+    os.system("git commit -am 'Added results for T=%f'" % T)
     os.system("git push origin master")
