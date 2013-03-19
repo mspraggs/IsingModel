c2239db5f3ef57837fa8a38bedd4b8de5376ff1e
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-19 22:49:34 +0000

    Changed number of iterations in the run script

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ Temps = [0.5*x for x in range(10)]
 for T in Temps:
     L = lattice.Lattice(n = 100,T=T,state=0)
     averages = []
-    for i in xrange(100):
+    for i in xrange(1000):
         print("Temperature: %f Step: %d" % (T,i))
         L.step()
         averages.append(L.spinaverage())
