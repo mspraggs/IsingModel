b10c08e5289b620c112bf5a20de05da79e2ad17c
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-19 22:51:47 +0000

    Added some sorting to the list of filenames

--- plot.py
+++ plot.py
@@ -5,13 +5,15 @@ from os.path import isfile, join
 
 files = [f for f in listdir("results") if isfile(join("results",f)) and f[-4:] == ".txt"]
 
+files.sort()
+
 pl.ion()
 
 for f in files:
     parameters = fileio.parsefilename(f)
     #This is where results could be filtered according to parameters if necessary
-    if True:
-        averages = fileio.readdata(f)
+    if parameters[3] in [0,1,2,3]:
+        averages = fileio.readdata(join("results",f))
         iterations = range(len(averages))
         label = "T=%f" % parameters[3]
         pl.plot(iterations,averages,label=label)
