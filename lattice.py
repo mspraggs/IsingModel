0ec744adbf444ef0faa9798cd5d998daab22c1e0
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-19 18:07:49 +0000

    Added code to set proportion of spins as up

--- lattice.py
+++ lattice.py
@@ -4,6 +4,15 @@ import pylab as pl
 
 class Lattice:
 
-    def __init__(self):
+    def __init__(self,n=10,state=0):
         """Constructor..."""
-        spins = 
+        self.n = n
+
+        self.spins = -1 * pl.ones(1,(n,n))
+
+        for i in xrange(n):
+            for j in xrange(n):
+                if pl.random() > (1 + state) / 2:
+                    self.spins[i,j] = 1
+
+        
