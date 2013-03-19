7f9be02101500604364a3365e6c9190d1e70d89f
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-19 22:27:40 +0000

    Added function to parse file names and some code to grab all the results files

--- plot.py
+++ plot.py
@@ -1,3 +1,18 @@
 import pylab as pl
 import fileio
+from os import listdir
+from os.path import isfile, join
 
+def parsefilename(filename):
+    """Parses file names to extract variables"
+    parameters = filename[:-4].split(",")
+    filterchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_="
+    return [eval(parameter.strip(filterchars)) for parameter in parameters]
+
+files = [f for f in listdir("results") if isfile(join("results",f)) and f[-4:] == ".txt"]
+
+for f in files:
+    parameters = parsefilename(f)
+    #This is where results could be filtered according to parameters if necessary
+    if True:
+        
