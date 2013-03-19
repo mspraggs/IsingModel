7c690d507781f1ea78552ef74e3f88eb023d4410
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-19 20:48:32 +0000

    Added logging of spin averages

--- run.py
+++ run.py
@@ -4,5 +4,7 @@ Temps = [0.5*x for x in range(10)]
 
 for T in Temps:
     L = lattice.Lattice(n = 100,T=T)
+    averages = []
     for i in xrange(1000):
         L.step()
+        averages.append(L.spinaverage())
