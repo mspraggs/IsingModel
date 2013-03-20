fcd6467b92cda784f2d424ade68665968e2559e6
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-20 17:47:41 +0000

    Added results for n=10,init=0.000000,J=1.000000,T=0.010000

--- run.py
+++ run.py
@@ -8,11 +8,15 @@ states = [0]
 Js = [1.]
 params = [(n,state,J,T) for n in ns for state in states for J in Js for T in Ts ]
 
+Saverage = []
+
 for param in params:
     L = lattice.Lattice(n = param[0],T=param[3],state=param[1],J=param[2])
+    Saverage.append(L.spinaverage())
     for i in xrange(100000):
         print("Run:%s Equilibrating: %d" % (L.config(),i))
         L.step()
+        Saverage.append(L.spinaverage())
 
     Etotals = []
     Stotals = []
