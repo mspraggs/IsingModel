1db28f8f5f575677b8686f69cb9a25d994e7cf6f
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-20 15:35:34 +0000

    Added results for n=50,init=0.000000,J=1.000000,T=0.010000

--- run.py
+++ run.py
@@ -2,15 +2,15 @@ import lattice
 import fileio
 import os
 
-Ts = [0.01] + [0.25*x for x in xrange(1,41)]
-ns = [100]
+Ts = [0.01*(x+1) for x in xrange(0,500)]
+ns = [50]
 states = [0]
 Js = [1.]
 params = [(n,state,J,T) for n in ns for state in states for J in Js for T in Ts ]
 
 for param in params:
     L = lattice.Lattice(n = param[0],T=param[3],state=param[1],J=param[2])
-    for i in xrange(10000000):
+    for i in xrange(100000):
         print("Run:%s Equilibrating: %d" % (L.config(),i))
         L.step()
 
