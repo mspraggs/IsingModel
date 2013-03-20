f4f14c011969c34d7c25ddab5bb041ecb5a249fa
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-20 21:33:22 +0000

    Used pylab variance funciton to calculate chi and Cv

--- plot.py
+++ plot.py
@@ -15,18 +15,18 @@ Ts = []
 
 for f in files:
     n,state,J,T = fileio.parsefilename(f)
-    Ts = [0.25*x for x in range(41)]
-    ns = [100]
+    Ts = [0.01*(x+1) for x in xrange(0,30)]
+    ns = [50]
     Js = [1]
-    states = [0]
+    states = [1]
     #This is where results could be filtered according to parameters if necessary
     if fileio.checkparameters([ns,states,Js,Ts],[n,state,J,T]):
         Etotals,Stotals = fileio.readdata(join("results",f))
         Eaverages = pl.array(Etotals) / n**2
         Saverages = pl.array(Stotals) / n**2
 
-        chi.append(1/T*(pl.mean(Saverages**2) - pl.mean(Saverages)**2))
-        Cv.append(1/T**2*(pl.mean(Eaverages**2) - pl.mean(Saverages)**2))
+        chi.append(1/T*pl.var(Saverages))
+        Cv.append(1/T**2*pl.var(Eaverages))
 
-pl.plot(Ts,chi)
+pl.plot(Ts,Cv)
 pl.show()
