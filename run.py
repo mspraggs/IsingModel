e95b26998dec9dae2ef69e8f3031929637a75c0e
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-20 17:19:33 +0000

    Added results for n=10,init=0.000000,J=1.000000,T=0.010000

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ params = [(n,state,J,T) for n in ns for state in states for J in Js for T in Ts 
 
 for param in params:
     L = lattice.Lattice(n = param[0],T=param[3],state=param[1],J=param[2])
-    for i in xrange(10000):
+    for i in xrange(100000):
         print("Run:%s Equilibrating: %d" % (L.config(),i))
         L.step()
 
