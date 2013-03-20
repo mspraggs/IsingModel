3de11b86d0f51cb6167523fdf6c45968b28f2a4e
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-20 02:06:41 +0000

    Updated parameter order to reflect file naming etc

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ Ts = [0.25*x for x in range(21)]
 ns = [100,200,300]
 states = [0.25*x - 1 for x in range(9)]
 Js = [0.5, 1., 1.5, 2.]
-params = [(n,T,state,J) for n in ns for T in Ts for state in states for J in Js]
+params = [(n,state,J,T) for n in ns for T in Ts for state in states for J in Js]
 
 for param in params:
-    L = lattice.Lattice(n = param[0],T=param[1],state=param[2],J=param[3])
+    L = lattice.Lattice(n = param[0],T=param[3],state=param[1],J=param[2])
     averages = []
     for i in xrange(10000):
         print("Run:%s Step: %d" % (L.config(),i))
