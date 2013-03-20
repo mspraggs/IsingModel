66f46a5759431766d5862984d4565cd5b80330de
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-20 10:42:12 +0000

    Added results for n=100,init=-1.000000,J=1.000000,T=0.000000

--- run.py
+++ run.py
@@ -2,11 +2,11 @@ import lattice
 import fileio
 import os
 
-Ts = [0.25]#[0.25*x for x in range(21)]
+Ts = [0.25*x for x in range(21)]
 ns = [100]
 states = [0.5*x - 1 for x in range(5)]
 Js = [1.]
-params = [(n,state,J,T) for n in ns for state in states for J in Js for T in Ts ]
+params = [(n,state,J,T) for T in Ts for J in Js for state in states for n in ns]
 
 for param in params:
     L = lattice.Lattice(n = param[0],T=param[3],state=param[1],J=param[2])
