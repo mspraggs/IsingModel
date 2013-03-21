e7e0d183bdffa2363623f6f1502e2b7e1d1e5c71
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-21 20:03:25 +0000

    Altered plot to list available results files before extracting data and dropping user to ipython prompt to enable plotting

--- plot.py
+++ plot.py
@@ -4,39 +4,19 @@ import fileio
 from os import listdir
 from os.path import isfile, join
 
-files = [f for f in listdir("results") if isfile(join("results",f)) and f[-4:] == ".txt"]
+files = [f for f in listdir("results") if isfile(join("results",f)) and f[:7] == "results"]
 
-files.sort()
+if len(files) > 0:
+    print("Available data:")
+    for i in xrange(len(files)):
+        print("(%d) %s" % (i,f))
 
-pl.ion()
+    file_num = input("File: ")
+    filename = "results/%s" % files[i]
 
-chi = []
-Cv = []
-Smean = []
-Emean = []
-Ts = []
-J1 = 0
+    T,E,M,Mtheory,Cv,chi = fileio.readdata(filename)
+    
+    IPython.embed()
 
-for f in files:
-    n,state,J,T = fileio.parsefilename(f)
-    J1 = J
-    Ts = [pl.around(0.01*(x+1),3) for x in range(0,500)]
-    ns = [20]
-    Js = [1]
-    states = [1]
-    #This is where results could be filtered according to parameters if necessary
-    if fileio.checkparameters([ns,states,Js,Ts],[n,state,J,T]):
-        Etotals,Stotals = fileio.readdata(join("results",f))
-        Eaverages = pl.array(Etotals) / n**2
-        Saverages = pl.array(Stotals) / n**2
-
-        chi.append(1/T*pl.var(Saverages))
-        Cv.append(1/T**2*pl.var(Eaverages))
-        Smean.append(pl.absolute(pl.mean(Saverages)))
-
-Tc = 2*float(J1)/pl.log(1.+pl.sqrt(2.))
-
-Stheory = [(1 - (pl.sinh(pl.log(1+pl.sqrt(2.))*Tc/T))**(-4))**(1./8) for T in Ts if T < Tc]
-Stheory += [0 for T in Ts if T >= Tc]
-
-IPython.embed()
+else:
+    print("No data available yet! Use postprocess.py to generate some.")
