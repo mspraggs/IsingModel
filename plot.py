bdb10e5b702f996a382f623cdc7cf78ea3f3c48f
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-23 10:20:14 +0000

    Fixed bug in file referencing

--- plot.py
+++ plot.py
@@ -5,11 +5,12 @@ from os import listdir
 from os.path import isfile, join
 
 files = [f for f in listdir("results") if isfile(join("results",f)) and f[:7] == "results"]
+IPython.embed()
 
 if len(files) > 0:
     print("Available data:")
     for i in xrange(len(files)):
-        print("(%d) %s" % (i,f))
+        print("(%d) %s" % (i,files[i]))
 
     file_num = input("File: ")
     filename = "results/%s" % files[i]
