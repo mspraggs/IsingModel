44dccb8db0839c13b40a0de45572a1a0b85dc87b
Matt Spraggs <matthew.spraggs@gmail.com>
2013-03-25 22:55:46 +0000

    Removed unnecessary flushing of stdout

--- run.py
+++ run.py
@@ -5,9 +5,11 @@ import os,sys
 def runparams(param,n_equil=500000,n_calc=250000):
     L = lattice.Lattice(n = param[0],T=param[3],state=param[1],J=param[2])
     print("Run:%s Equilibrating..." % L.config)
+    sys.stdout.flush()
     for i in xrange(n_equil):
         L.cstep()
     print("Done!")
+    sys.stdout.flush()
 
     Etotals = []
     Stotals = []
@@ -17,19 +19,20 @@ def runparams(param,n_equil=500000,n_calc=250000):
     Stotals.append(Stotal)
 
     print("Run:%s Calculating..." % L.config)
+    sys.stdout.flush()
     for i in xrange(n_calc):
         Ediff,Sdiff = L.cstep()
         Etotal += Ediff
-        sys.stdout.flush()
         Stotal += Sdiff
         Etotals.append(Etotal)
         Stotals.append(Stotal)
 
     print("Done!")
+    sys.stdout.flush()
 
     fileio.writedata("results/%s.txt" % L.config,[Etotals,Stotals])
 
-Ts = [0.01*(x+1) for x in xrange(0,500)]
+Ts = [5.]#[0.01*(x+1) for x in xrange(0,500)]
 ns = [20]
 states = [1]
 Js = [1.]
